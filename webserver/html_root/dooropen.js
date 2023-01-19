f0e18c7664dfb480200b8c528bd7570abb6a4b50
Majorx234 <majorx234@googlemail.com>
2023-01-19 03:41:25 +0100

    Add test functionality for REST API

--- webserver/html_root/dooropen.js
+++ webserver/html_root/dooropen.js
@@ -1,6 +1,6 @@
 function getServerAddress() {
   server_ip = document.getElementById("server_id").value;
-  var server_adress = 'http://'.concat(server_ip,':5000');
+  var server_adress = 'http://'.concat(server_ip,':8080');
   return server_adress;
 }
 
@@ -12,22 +12,38 @@ function outputToConsole(text) {
     para.scrollIntoView();
 }
 
+function serveGetRequest(endpoint) {
+      var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        outputToConsole(this.responseText);
+      }
+    };
+    var ping_endpoint = "".concat(getServerAddress(),endpoint);
+    xhttp.open("GET", ping_endpoint, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(null);
+}
+
 class DoorapiREST {
   constructor(){
     document.getElementById("server_id").setAttribute('value','127.0.0.1');
   }
-  get_status(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        alert(this.responseText);
-      }
-    };
-    var ping_endpoint = "".concat(getServerAddress(),'/v1.0/ping');
-    xhttp.open("GET", ping_endpoint, true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(midi_msg); 
+  ping(){
+    outputToConsole('ping');
+    serveGetRequest('/v1.0/ping');
+  }
+  get_door_status(){
+    outputToConsole('get door_status:');
+    serveGetRequest('/v1.0/door_status');
   }
 }
-
+outputToConsole("init");
 let dooropen_api = new DoorapiREST();
+document.getElementById('server_ping').onclick = function(){
+  dooropen_api.ping();
+}
+document.getElementById('server_door_status').onclick = function() {
+  dooropen_api.get_door_status();
+};
+outputToConsole("running...");
